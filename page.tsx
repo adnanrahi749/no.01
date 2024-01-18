00f6f897786664b77cf42681d11644caeb4e2792
Muhammad Adnan Rahi <144876672+adnanrahi749@users.noreply.github.com>
2024-01-17 22:30:15 -0800

    Add files via upload
    no.01

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-5xl bg-yellow-200 text-center '>
+      Muhammad Adnan Rahi
+    </div>
+  )
+}
+
+export default page
